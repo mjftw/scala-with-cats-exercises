930ce80f87a0f68c0b3cb96b2dd9f4bc4631f183
Merlin Webster <merlin.webster@kaluza.com>
2021-04-16 13:46:47 +0100

    Created project with "sbt new scalawithcats/cats-seed.g8"

---
+++ .ammonite/scala-2.12.13/amm-2.3.8-58-aa8b2ab1/test/src/ammonite/$file/test.scala
@@ -0,0 +1,80 @@
+
+package ammonite
+package $file
+import _root_.ammonite.interp.api.InterpBridge.{
+  value => interp
+}
+import _root_.ammonite.interp.api.InterpBridge.value.{
+  exit
+}
+import _root_.ammonite.interp.api.IvyConstructor.{
+  ArtifactIdExt,
+  GroupIdExt
+}
+import _root_.ammonite.compiler.CompilerExtensions.{
+  CompilerInterpAPIExtensions,
+  CompilerReplAPIExtensions
+}
+import _root_.ammonite.runtime.tools.{
+  browse,
+  grep,
+  time,
+  tail
+}
+import _root_.ammonite.compiler.tools.{
+  desugar,
+  source
+}
+import _root_.mainargs.{
+  arg,
+  main
+}
+import _root_.ammonite.repl.tools.Util.{
+  PathRead
+}
+
+
+object test{
+/*<script>*/sealed trait Json
+final case class JsObject(get: Map[String, Json]) extends Json
+final case class JsString(get: String) extends Json
+final case class JsNumber(get: Double) extends Json
+final case object JsNull extends Json
+
+// The "serialize to JSON" behaviour is encoded in this trait
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
+
+final case class Person(name: String, email: String)
+
+object JsonWriterInstances {
+  implicit val stringWriter: JsonWriter[String] =
+    new JsonWriter[String] {
+      def write(value: String): Json =
+        JsString(value)
+    }
+
+  implicit val personWriter: JsonWriter[Person] =
+    new JsonWriter[Person] {
+      def write(value: Person): Json =
+        JsObject(
+          Map(
+            "name" -> JsString(value.name),
+            "email" -> JsString(value.email)
+          )
+        )
+    }
+
+  // etc...
+}
+
+object Json {
+  def toJson[A](value: A)(implicit w: JsonWriter[A]): Json =
+    w.write(value)
+}
+/*</script>*/ /*<generated>*/
+def $main() = { scala.Iterator[String]() }
+  override def toString = "test"
+  /*</generated>*/
+}
