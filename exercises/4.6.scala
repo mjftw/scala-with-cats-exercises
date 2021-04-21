c817697e787b52ddc918b4bd231ed45dbf725f96
Merlin Webster <merlin.webster@kaluza.com>
2021-04-21 12:35:34 +0100

    Copied Ex 4.6 solution from book

---
+++ exercises/4.6.scala
@@ -0,0 +1,18 @@
+import cats.Eval
+
+object Main {
+
+  def foldRightEval[A, B](as: List[A], acc: Eval[B])(
+      fn: (A, Eval[B]) => Eval[B]
+  ): Eval[B] = as match {
+    case head :: tail =>
+      Eval.defer(fn(head, foldRightEval(tail, acc)(fn)))
+    case Nil =>
+      acc
+  }
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    foldRightEval(as, Eval.now(acc)) { (a, b) =>
+      b.map(fn(a, _))
+    }.value
+}
